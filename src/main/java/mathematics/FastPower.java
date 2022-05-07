6d84c30ab3d30d02ce31093346e3538eee17f484
varunsharma97 <varuntrustme97@gmail.com>
2022-05-07 22:34:09 +0530

    Added fastpower recursive impl

--- src/main/java/mathematics/FastPower.java
+++ src/main/java/mathematics/FastPower.java
@@ -5,7 +5,7 @@ public class FastPower {
      * Naive approach to find power of x^n
      * O(n) solution
      */
-    static long powerNaive(int x, int n){
+    static long power(int x, int n){
         int res = 1;
 
         for (int i=0; i<n; i++)
@@ -15,10 +15,27 @@ public class FastPower {
     }
 
     /***
+     * An optimised recursive solution
+     * The approach is that every even power n can be represented as x^n = (x^n/2)^2
+     * and odd power n can be represented as x^n = x*(x^n/2)^2
+     * Time Complexity of this solution is O(log(n))
+     */
+    static long fastPower(int x, int n){
+        //base case
+        if(n==0) return 1;
+        long temp = fastPower(x, n/2);
+        temp *= temp;
+        if(n%2!=0) temp = x*temp;
+
+        return temp;
+    }
+
+    /***
      * Driver code
      */
     public static void main(String[] args) {
         int x=2, n=5;
-        System.out.println(x+"^"+n+"="+powerNaive(x,n));
+        System.out.println(x+"^"+n+"="+ power(x,n));
+        System.out.println(x+"^"+n+"="+fastPower(x,4));
     }
 }
